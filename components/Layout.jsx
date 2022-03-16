1cda730c40831aa24f9266b04715b47d2488b82f
phanmhung <pmhungpro1@gmail.com>
2022-03-16 15:11:36 +0700

    react context

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
 
@@ -10,11 +10,17 @@ import {
   Link,
   CssBaseline,
   ThemeProvider,
+  Switch,
 } from '@material-ui/core';
 import useStyles from '../utils/styles';
 import { createTheme } from '@material-ui/core/styles';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
 
 export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+
   const theme = createTheme({
     typography: {
       h1: {
@@ -32,7 +38,7 @@ export default function Layout({ title, description, children }) {
       },
     },
     palette: {
-      type: 'light',
+      type: darkMode ? 'dark' : 'light',
       primary: {
         main: '#f0c000',
       },
@@ -41,6 +47,11 @@ export default function Layout({ title, description, children }) {
       },
     },
   });
+  const handleDarkModeChange = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
   return (
     <div>
       <Head>
@@ -60,6 +71,10 @@ export default function Layout({ title, description, children }) {
             </NextLink>
             <div className={useStyles().grow}></div>
             <div>
+              <Switch
+                checked={darkMode}
+                onChange={handleDarkModeChange}
+              ></Switch>
               <NextLink href="/cart" passHref>
                 Cart
               </NextLink>
