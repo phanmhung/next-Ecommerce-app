4a1880338b3a94ae4502194b6e461949850a05a7
phanmhung <pmhungpro1@gmail.com>
2022-03-18 10:29:35 +0700

    fix card list actions flew down when reload

--- pages/cart.js
+++ pages/cart.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import dynamic from 'next/dynamic'; //lib module on server-side
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import NextLink from 'next/link';
@@ -6,6 +7,7 @@ import {
   Grid,
   Typography,
   TableContainer,
+  Table,
   TableHead,
   TableRow,
   TableCell,
@@ -15,11 +17,12 @@ import {
   MenuItem,
   Button,
   Card,
+  List,
   ListItem,
 } from '@material-ui/core';
 import Image from 'next/image';
 
-export default function CartScreen() {
+function CartScreen() {
   const { state } = useContext(Store);
   const {
     cart: { cartItems },
@@ -37,71 +40,75 @@ export default function CartScreen() {
         <Grid container spacing={1}>
           <Grid item md={9} xs={12}>
             <TableContainer>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Image</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell align="right">Quantity</TableCell>
-                  <TableCell align="right">Price</TableCell>
-                  <TableCell align="right">Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {cartItems.map((item) => (
-                  <TableRow key={item._id}>
-                    <TableCell>
-                      <NextLink href={`/product/${item.slug}`} passHref>
-                        <Link>
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            width={50}
-                            height={50}
-                          ></Image>
-                        </Link>
-                      </NextLink>
-                    </TableCell>
-
-                    <TableCell>
-                      <NextLink href={`/product/${item.slug}`} passHref>
-                        <Link>
-                          <Typography>{item.name}</Typography>
-                        </Link>
-                      </NextLink>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Select value={item.quantity}>
-                        {[...Array(item.countInStock).keys()].map((x) => (
-                          <MenuItem key={x + 1} value={x + 1}>
-                            {x + 1}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </TableCell>
-                    <TableCell align="right">$ {item.price}</TableCell>
-                    <TableCell align="right">
-                      <Button variant="contained" color="secondary">
-                        x
-                      </Button>
-                    </TableCell>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Image</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Quantity</TableCell>
+                    <TableCell align="right">Price</TableCell>
+                    <TableCell align="right">Action</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
+                </TableHead>
+                <TableBody>
+                  {cartItems.map((item) => (
+                    <TableRow key={item._id}>
+                      <TableCell>
+                        <NextLink href={`/product/${item.slug}`} passHref>
+                          <Link>
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={50}
+                              height={50}
+                            ></Image>
+                          </Link>
+                        </NextLink>
+                      </TableCell>
+
+                      <TableCell>
+                        <NextLink href={`/product/${item.slug}`} passHref>
+                          <Link>
+                            <Typography>{item.name}</Typography>
+                          </Link>
+                        </NextLink>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Select value={item.quantity}>
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <MenuItem key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </TableCell>
+                      <TableCell align="right">$ {item.price}</TableCell>
+                      <TableCell align="right">
+                        <Button variant="contained" color="secondary">
+                          x
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </TableContainer>
           </Grid>
-          <Grid md={3} xs={12}>
+          <Grid item md={3} xs={12}>
             <Card>
-              <ListItem>
-                <Typography variant="h2">
-                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : $
-                  {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Button variant="contained" color="primary" fullWidth>
-                  Check out
-                </Button>
-              </ListItem>
+              <List>
+                <ListItem>
+                  <Typography variant="h2">
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) :
+                    $ {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Button variant="contained" color="primary" fullWidth>
+                    Check out
+                  </Button>
+                </ListItem>
+              </List>
             </Card>
           </Grid>
         </Grid>
@@ -109,3 +116,4 @@ export default function CartScreen() {
     </Layout>
   );
 }
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
